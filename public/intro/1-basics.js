305154124b832253fc7355e328e875d9c429e884
Omar Imam <flowmar@kineticvortex.com>
2018-02-04 12:42:17 -0600

    Fix three.js library

--- public/intro/1-basics.js
+++ public/intro/1-basics.js
@@ -1,22 +1,42 @@
+//! Create variables
 var scene = new THREE.Scene();
 var aspect = window.innerWidth / window.innerHeight;
 var camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-
+//! Append domElement to DOM
 document.getElementById('webgl-container').appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1);
-var material = new THREE.MeshNormalMaterial();
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Create a new geometry, material and apply it to the cube mesh
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var geometry2 = new THREE.BoxGeometry(2, 2, 2);
+var material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  wireframe: true
+});
+var cubeA = new THREE.Mesh(geometry, material);
+cubeA.position.set(100, 100, 0);
+
+var cubeB = new THREE.Mesh(geometry2, material);
+cubeB.position.set(-100, -100, 0);
+
+//!Set the camera position
 camera.position.z = 5;
+
+//!.Create a new group
+var group = new THREE.Group;
+scene.add(group);
+group.add(cubeA);
+group.add(cubeB);
+
+
+
 
 var render = function() {
   requestAnimationFrame(render);
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  cubeA.rotation.x += 0.1;
+  cubeB.rotation.y += 0.1;
   renderer.render(scene, camera);
 }
 
